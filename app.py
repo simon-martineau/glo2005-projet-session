4dbc1a859a07196b15171259e8d1c026ad3c4b61
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-04-17 13:19:32 -0400

    someCommit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from conf import BasicConfig
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,11 +1,18 @@
-CREATE TABLE users
+CREATE TABLE user
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50),
     last_name  VARCHAR(50),
     username   VARCHAR(50),
+    email      VARCHAR(50),
     birth_date DATE
 );
+
+CREATE TABLE item
+(
+
+);
+
 CREATE TABLE buyers
 (
     user_id INT PRIMARY KEY,
@@ -13,15 +20,10 @@ CREATE TABLE buyers
         REFERENCES users (id)
         ON DELETE CASCADE
 );
-CREATE TABLE sellers(
+CREATE TABLE sellers
+(
     user_id INT PRIMARY KEY,
     FOREIGN KEY (user_id)
 )
 ;
-CREATE TABLE users
-(
-    first_name VARCHAR,
-    last_name  VARCHAR,
-    username   VARCHAR,
-    birth_date DATE
-);
+
