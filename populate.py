6145bd564e1b4e01cd3ca9e75e3c8e6b5d1cdfcf
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-04-20 14:43:38 -0400

    yeet

--- populate.py
+++ populate.py
@@ -6,7 +6,6 @@ import uuid
 import random
 import datetime
 
-
 ENCRYPTED_PASSWORD = "$5$rounds=535000$7/J2PCRN7CEXcC5/$prCsIeitr4L6jkEs/xNswqg8oVEHmJn9tYVYlVXT.DA"
 
 
@@ -39,31 +38,35 @@ def populate() -> None:
     buyers_last_names = [names.get_last_name() for _ in range(nb_of_buyers)]
     buyers_usernames = [buyers_first_names[x][:4] + buyers_last_names[x][:4] for x in range(nb_of_buyers)]
     start_date = datetime.date(1971, 1, 1)
-    buyers_birth_dates = [(start_date+datetime.timedelta(days=random.randint(1, 9000))).strftime("%Y-%m-%d") for _ in range(nb_of_buyers)]
+    buyers_birth_dates = [(start_date + datetime.timedelta(days=random.randint(1, 9000))).strftime("%Y-%m-%d") for _ in
+                          range(nb_of_buyers)]
 
     # Sellers
     nb_of_sellers = 100
     sellers_user_ids = users_ids[101:]
     sellers_first_names = [names.get_first_name() for _ in range(10)]
-    sellers_last_names = ["store", "boutique", "market", "company", "items", "variety", "selection", "shop", "treasures", "picks"]
+    sellers_last_names = ["store", "boutique", "market", "company", "items", "variety", "selection", "shop",
+                          "treasures", "picks"]
     sellers_names = ["'s ".join(x) for x in list(itertools.product(sellers_first_names, sellers_last_names))]
     sellers_descriptions = [f"Find everything at {x}!" for x in sellers_names]
 
     # Items
     colors = ["Red", "Orange", "Yellow", "Lime green", "Green", "Teal", "Blue", "Purple", "Pink", "White", "Black"]
-    products = ["chair", "dining table", "couch", "pillow", "coffee table", "bookshelf", "ottoman", "desk", "rug", "bedsheet set"]
-    nb_of_items = len(colors)*len(products)
+    products = ["chair", "dining table", "couch", "pillow", "coffee table", "bookshelf", "ottoman", "desk", "rug",
+                "bedsheet set"]
+    nb_of_items = len(colors) * len(products)
     item_ids = [str(uuid.uuid4()) for _ in range(nb_of_items)]
     items_names = [" ".join(x) for x in list(itertools.product(colors, products))]
     items_descriptions = [" ".join(["A beautiful", x, "that fits all your needs!"]) for x in items_names]
     items_prices = [round(random.uniform(15.0, 65.0), 2) for _ in range(nb_of_items)]
     items_quantity = [random.randint(5, 20) for _ in range(nb_of_items)]
-    items_categories = ["Furniture" for _ in range (nb_of_items)]
+    items_categories = ["Furniture" for _ in range(nb_of_items)]
     items_seller_ids = sellers_user_ids
 
     # Comments
     nb_of_comments = 100
-    adverbs = ["Really", "Not really", "Very", "Not very", "Immensely", "Not immensely", "Intensely", "Not intensely", "Humongously", "Not humongously"]
+    adverbs = ["Really", "Not really", "Very", "Not very", "Immensely", "Not immensely", "Intensely", "Not intensely",
+               "Humongously", "Not humongously"]
     adjectives = ["good", "bad", "convenient", "useful", "futile", "useless", "lovely", "cool", "nice", "awesome"]
     comment_ids = [str(uuid.uuid4()) for _ in range(nb_of_comments)]
     comments_content = [" ".join(x) for x in list(itertools.product(adverbs, adjectives))]
@@ -91,7 +94,7 @@ def populate() -> None:
 
     # Populating Sellers
     for i in range(nb_of_sellers):
-        sellers_query = f"INSERT INTO Sellers (user_id, name, description) VALUES ('{users_ids[i+100]}', '{sellers_names[i]}', '{sellers_descriptions[i]}');"
+        sellers_query = f"INSERT INTO Sellers (user_id, name, description) VALUES ('{users_ids[i + 100]}', '{sellers_names[i]}', '{sellers_descriptions[i]}');"
         client.query_none(sellers_query)
 
     # Populating Items
@@ -107,3 +110,7 @@ def populate() -> None:
     # Populating Transactions
     for i in range(nb_of_transaction):
         transactions_query = f"INSERT INTO Transactions (transaction_id, buyer_id, seller_id, item_id, price, quantity) VALUES ('{transaction_ids[i]}', '{buyers_user_ids[i]}', '{sellers_user_ids[i]}', '{item_ids[i]}', {items_prices[i]}, {transactions_quantities[i]});"
+
+
+if __name__ == '__main__':
+    populate()
